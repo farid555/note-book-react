92db1ecc916871177da174756628ffe2cd0082b8
farid555 <farid555a@gmail.com>
2022-02-14 16:32:59 +0200

    adding ispending & Error

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,20 +3,45 @@ import { useState, useEffect } from "react";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [ispending, setIspending] = useState(true);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-      });
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Could not fetch the dat from that resource...");
+          } else {
+            return res.json();
+          }
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIspending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setIspending(false);
+        });
+    }, 1000);
+
     console.log("test");
   }, []);
 
   return (
-    <div>
+    <div className="home">
+      {ispending && (
+        <div>
+          <h2>Loading...</h2>
+        </div>
+      )}
+      {error && (
+        <div>
+          <h2>{error}</h2>
+        </div>
+      )}
       <BlogList blogs={blogs} />
     </div>
   );
