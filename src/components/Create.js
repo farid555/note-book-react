eeb6e3f438d60dd26caa6a20e94e2a3ecb62993f
farid555 <farid555a@gmail.com>
2022-02-14 20:00:11 +0200

    adding form (handleSubmit)

--- src/components/Create.js
+++ src/components/Create.js
@@ -7,10 +7,16 @@ const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+    console.log(blog);
+  };
   return (
     <div className="create">
       <h2>Add new blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog Title:</label>
         <input
           type="text"
