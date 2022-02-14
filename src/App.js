48dd8f09adde6cc1115903bc0fe7612ea64088e3
farid555 <farid555a@gmail.com>
2022-02-14 12:54:41 +0200

    title, like

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 
 const App = () => {
+  const title = "Welcome to new React NoteBook";
+  const like = 50;
+
   return (
     <div className="App">
       <div className="content">
-        <h1>Hello React!</h1>
+        <h1>{title}</h1>
+        <p>Liked {like} times...</p>
       </div>
     </div>
   );
