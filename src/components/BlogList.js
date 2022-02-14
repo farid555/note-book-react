d16e98d4a6cc7ece1da760d9b4d319289f2885f6
farid555 <farid555a@gmail.com>
2022-02-14 15:58:05 +0200

    fetching data inside of useEffect

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,19 +1,19 @@
 import React from "react";
 
-const BlogList = ({ blogs, handleDelete }) => {
+const BlogList = ({ blogs }) => {
   return (
     <div>
       <h1>HomePage</h1>
       <div>
-        {blogs.map((blog) => {
-          return (
-            <div className="blog-previews" key={blog.id}>
-              <h2>{blog.title}</h2>
-              <p> Written by {blog.author}</p>
-              <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
-            </div>
-          );
-        })}
+        {blogs &&
+          blogs.map((blog) => {
+            return (
+              <div className="blog-previews" key={blog.id}>
+                <h2>{blog.title}</h2>
+                <p> Written by {blog.author}</p>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,45 +2,22 @@ import BlogList from "./BlogList";
 import { useState, useEffect } from "react";
 
 const Home = () => {
-  const [blogs, setBlog] = useState([
-    {
-      title: "THE JOURNEY OF THE MARMABILL",
-      body: "For those spending time at home, looking for a cozy adventure or bedtime story for kids, we offer this imaginative rhyming picture book, read by the author, Daniel Errico.",
-      author: "Aaniel Errico",
-      id: 1,
-    },
-    {
-      title: "GEMMA",
-      body: "Gemma is a middle grade novel that follows a curious explorer and her ring-tailed lemur, Milo, as they hunt for the “most greatest treasure in the world”. Solving riddles, battling a bell-wearing jaguar.",
-      author: "Daniel Errico",
-      id: 2,
-    },
-    {
-      title: "THE GUARDIANS OF LORE",
-      body: "While drawing in class at Stagwood School, 12-year old Cal sees a frog staring at him through the window. Stranger than that, is the fact that this frog happens to be wearing glasses. Cal and his best friend, Soy, ",
-      author: "Daniel Errico",
-      id: 3,
-    },
-    {
-      title: "HE PARTICULAR WAY OF THE ODD MS. MCKAY",
-      body: "This quirky tale is about our most infamous character of all. If you don’t have a soufflé-baking, trumpet-blaring, sleigh-riding friend, then maybe it’s time that you met Ms. McKay. Told as a monorhyme poem.",
-      author: "Daniel Errico",
-      id: 4,
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
   useEffect(() => {
-    console.log("useEffect Run");
-    console.log(blogs);
-  });
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+      });
+    console.log("test");
+  }, []);
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlog(newBlogs);
-  };
   return (
     <div>
-      <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
+      <BlogList blogs={blogs} />
     </div>
   );
 };
