72b54b238ffcafb9c228f803bcdf102a0c6da1c3
farid555 <farid555a@gmail.com>
2022-02-14 13:00:08 +0200

    dynamic value

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import React from "react";
 const App = () => {
   const title = "Welcome to new React NoteBook";
   const like = 50;
+  const link = "https://github.com/";
 
   return (
     <div className="App">
       <div className="content">
         <h1>{title}</h1>
         <p>Liked {like} times...</p>
+        <p> {10}</p>
+        <p> [1, 2, 3, 4]</p>
+        <p>{Math.random() * 10 + 1}</p>
+        <a href={link}> Github</a>
       </div>
     </div>
   );
